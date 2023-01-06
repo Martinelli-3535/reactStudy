3dcb98fbcade6ed737f2f31a45929ba082b9ddd6
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2023-01-06 10:16:24 +0900

    2023 01 06

--- web/src/api/Weather.js
+++ web/src/api/Weather.js
@@ -1,39 +1,33 @@
-import instance, {SERVICE_KEY} from '../utils/axiosInstance';
+import instance, { SERVICE_KEY } from "../utils/AxiosInstance";
 
-export const getUltraSrtNcst =
-    async (param = {
-        serviceKey: SERVICE_KEY,
-        pageNo: 1,
-        numOfRows: 1000,
-        dataType: 'JSON',
-        base_date: new Date().format('yyyyMMdd'),
-        base_time: `${new Date().format('HH')}00`,
-        nx: 54,
-        ny: 123
-    }) => {
-    const response = await instance.get(
-        `/getUltraSrtNcst`,
-        {params: param}
-    );
-    return response.data;
-}
+export const getUltraSrtNcst = async (
+  param = {
+    serviceKey: SERVICE_KEY,
+    pageNo: 1,
+    numOfRows: 1000,
+    dataType: "JSON",
+    base_date: new Date().format("yyyyMMdd"),
+    base_time: `${new Date().format("HH")}00`,
+    nx: 54,
+    ny: 123,
+  }
+) => {
+  const response = await instance.get(`/getUltraSrtNcst`, { params: param });
+  return response.data;
+};
 
-export const getVilageFcst = 
-    async (param = {
-        serviceKey: SERVICE_KEY,
-        pageNo: 1,
-        numOfRows: 1000,
-        dataType: 'JSON',
-        base_date: new Date().format('yyyyMMdd'),
-        base_time: `${new Date().format('HH')}00`,
-        nx: 54,
-        ny: 123
-    }) => {
-const response = await instance.get(
-        `/getVilageFcst`,
-        {params: param}
-    );
-    return response.data;
-}
-
-    
+export const getVilageFcst = async (
+  param = {
+    serviceKey: SERVICE_KEY,
+    pageNo: 1,
+    numOfRows: 1000,
+    dataType: "JSON",
+    base_date: new Date().format("yyyyMMdd"),
+    base_time: `${new Date().format("HH")}00`,
+    nx: 54,
+    ny: 123,
+  }
+) => {
+  const response = await instance.get(`/getVilageFcst`, { params: param });
+  return response.data;
+};
