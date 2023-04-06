3a6b1f7862f2f524ca57e2290839f1c91d75b4cd
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2023-04-06 17:45:55 +0900

    2023 04 06
    라우터 적용

--- practice-router/router-tuto/src/App.js
+++ practice-router/router-tuto/src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
+import React from 'react';
 import About from './pages/About';
 import Home from './pages/Home';
+import Profile from './pages/About';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
+      <Route path="/profiles/:username" element={<Profile />} />
     </Routes>
   )
 }
