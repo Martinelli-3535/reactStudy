0724b66c5abe98bf15235a796a9e1ead21f31724
Jungwoo Hong <79641160+Martinelli-3535@users.noreply.github.com>
2023-07-21 23:58:56 +0900

    2023 07 21
    redux-thunk

--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -10,5 +10,66 @@ const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 export const getPost = id => async dispatch => {
-    
-}
+    dispatch({type: GET_POST});
+    try {
+        const response = await api.getPost(id);
+        dispatch({
+            type: GET_POST_SUCCESS,
+            payload: response.data
+        });
+    } catch (e) {
+        dispatch({
+            type: GET_POST_FAILURE,
+            payload: e,
+            error: true
+        });
+        throw e;
+    }
+};
+
+export const getUsers = () => async dispatch => {
+    dispatch({type:GET_USERS}); // 요청을 시작한 것을 알림
+    try {
+        const response = await api.getUsers();
+        dispatch({
+            type:GET_USERS_SUCCESS,
+            payload: response.data
+        }); // 요청 성공
+    } catch (e) {
+        dispatch({
+            type: GET_USERS_FAILURE,
+            payload: e,
+            error: true
+        });
+        throw e; //나중에 컴포넌트단에서 에러를 조회할 수 있게 해줌
+    }
+};
+
+// 초기 상태 선언
+
+const initialState = {
+    loading: {
+        GET_POST: false,
+        GET_USERS: false
+    },
+    post: null,
+    users: null
+};
+
+const sample = handleActions(
+    {
+        [GET_POST]: state => ({
+            ...state,
+            loading: {
+                ...state.loading,
+                GET_POST: true
+            }
+        }),
+        [GET_POST_SUCCESS]: (state, action) => ({
+            ...state,
+            loading: {
+                ...state.
+            }
+        })
+    }
+)
